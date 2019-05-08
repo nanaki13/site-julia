cf7fdfc7d834d27082daac4a7ae9c35c047ee185
nanaki13 <jonathan.bonnet@laposte.net>
2019-05-08 13:15:11 +0200

    test herocku command

--- project/FrontendCommands.scala
+++ project/FrontendCommands.scala
@@ -3,8 +3,8 @@
   * Change these if you are using some other package manager. i.e: Yarn
   */
 object FrontendCommands {
-  val dependencyInstall: String = "npm install"
-  val test: String = "npm run test:ci"
-  val serve: String = "npm run start"
-  val build: String = "npm run build:prod"
+  val dependencyInstall: String = "heroku run npm install"
+  val test: String = "heroku run npm run test:ci"
+  val serve: String = "heroku run npm run start"
+  val build: String = "heroku run npm run build:prod"
 }
